aedc069f7f69d3a7b95c7c776b83a3dc7eb67d77
Gurhale Navjyot <navjyotgurhale@hotmail.com>
2022-04-09 07:25:54 +0530

    Add project files.

---
+++ DesignPattern/CreationalDesignPattern/FactoryDesignPattern.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.CreationalDesignPattern
+{
+    /*
+        - A factory is an object which is used for creating other objects
+        - In technical terms, we can say that a factory is a class with a method
+        - In the Factory Design pattern, we create an object without exposing the object creation logic to the client 
+          and the client will refer to the newly created object using a common interface.
+     */
+
+    public interface CreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+
+    class MoneyBack : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+
+    public class Titanium : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Titanium Edge";
+        }
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+    }
+
+    public class Platinum : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 2000;
+        }
+    }
+
+
+    class CreditCardFactory
+    {
+        public static CreditCard GetCreditCard(string cardType)
+        {
+            CreditCard cardDetails = null;
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                cardDetails = new Titanium();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new Platinum();
+            }
+            return cardDetails;
+        }
+    }
+
+
+    class Program
+    {
+
+        public static void ClientMethodWithoutFactoryPattern(string cardType)
+        {
+            //Generally we will get the Card Type from UI.
+            //Here we are hardcoded the card type
+            //string cardType = "MoneyBack";
+            CreditCard cardDetails = null;
+            //Based of the CreditCard Type we are creating the
+            //appropriate type instance using if else condition
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                cardDetails = new Titanium();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new Platinum();
+            }
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType : " + cardDetails.GetCardType());
+                Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+                Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+        }
+
+        public static void ClientMethodWithFactoryPattern(string cardType)
+        {
+            /*
+                Problem of the above code implementation
+                - First, the tight coupling between the client class (Program) and Product Class (MoneyBack, Titanium, and Platinum)
+                - Secondly, if we add a new Credit Card, then also we need to modify the client method by adding an extra if-else
+             */
+            CreditCard cardDetails = CreditCardFactory.GetCreditCard(cardType);
+
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType : " + cardDetails.GetCardType());
+                Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+                Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string cardType = "MoneyBack";
+            Console.WriteLine("============Without Factory============");
+            ClientMethodWithoutFactoryPattern(cardType); //Without Factory
+            Console.WriteLine("============With Factory============");
+            ClientMethodWithFactoryPattern(cardType); //With Factory
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+   - Design Patterns are reusable solutions to the problems that as a developer we encounter in our day to day programming. 
+   - Design Patterns are basically used to solve the problems of object generation and integration. 
+   
+   Types Of Design Pattern:
+    1. Creational Design Pattern (object creation and initialization)
+    2. Structural Design Pattern (Structural Changes of class and interfaces, and the relationship between classes)
+    3. Behavioral Design Pattern (Communication Between Objects)
+
+   Creational Design Patterns:
+   - The Creational Design Pattern deals with Object Creation and Initialization.
+   - Examples of Creational design patterns are 
+           - Factory
+           - Factory Method
+           - Abstract Factory
+           - Builder 
+           - Fluent Interface
+           - Prototype
+           - Singleton
+
+   Structural Design Patterns: 
+   - The Structural Design Pattern is basically used to manage the structure of classes and interface as well as manage the relationship between the classes.
+   - Examples of Structural Design Patterns category: 
+            - Adapter
+            - Facade
+            - Decorator
+            - Composite
+            - Proxy
+            - Flyweight
+            - Bridge Design Pattern
+
+   Behavioral Design Patterns: 
+   - The Behavioral Design Patterns deal with the communication between Classes and objects.
+   - Examples of Behavioral design patterns: 
+            - Chain of Responsibility
+            - Command
+            - Observer
+            - Iterator
+            - State
+            - Template Method
+            - Visitor
+            - Strategy
+            - Mediator
+            - Memento
+            - Interpreter Design Pattern
+ */
+
+Console.WriteLine("Hello, World!");
